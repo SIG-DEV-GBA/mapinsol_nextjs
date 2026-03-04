3fc452bef585393fbe8ea17521d59e0425e75c69
Georgi Borisov <g.borisov@solidaridadIntergeneracional.es>
2026-03-04 14:00:08 +0100

    fix: move GA scripts from head to body for proper loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,7 +107,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className={`${poppins.variable} ${nunito.variable} ${lato.variable}`}>
-      <head>
+      <body className="min-h-screen flex flex-col font-lato antialiased relative overflow-x-hidden" suppressHydrationWarning>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-Z222Z7CJWF" strategy="afterInteractive" />
         <Script id="gtag-init" strategy="afterInteractive">
           {`window.dataLayer = window.dataLayer || [];
@@ -115,8 +115,6 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-Z222Z7CJWF');`}
         </Script>
-      </head>
-      <body className="min-h-screen flex flex-col font-lato antialiased relative overflow-x-hidden" suppressHydrationWarning>
         {/* Fondo con gradiente radial */}
         <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]" />
 
